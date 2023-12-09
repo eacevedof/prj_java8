8425e0ebb9a5da13cf0720403ab724d326a52bf1
Eduardo A. F <eacevedof@yahoo.es>
2023-12-09 16:42:23 +0100

    dp creational

--- design-patterns/src/Creational/AbstractFactory/AlumnosRepositoryInterface.java
+++ design-patterns/src/Creational/AbstractFactory/AlumnosRepositoryInterface.java
@@ -3,5 +3,5 @@ package Creational.AbstractFactory;
 import java.util.List;
 
 public interface AlumnosRepositoryInterface {
-    public List<String> getListaAlumnos();
+    List<String> getListaAlumnos();
 }
