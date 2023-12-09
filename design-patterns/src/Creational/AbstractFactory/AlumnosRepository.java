f66229f941efaa62c6ecf7d07873dfe21a39e772
Eduardo A. F <eacevedof@yahoo.es>
2023-12-09 15:24:27 +0100

    dp rreadme

---
+++ design-patterns/src/Creational/AbstractFactory/AlumnosRepository.java
@@ -0,0 +1,10 @@
+package Creational.AbstractFactory;
+
+import java.util.List;
+
+public class AlumnosRepository implements AlumnosRepositoryInterface{
+    @Override
+    public List<String> getListaAlumnos() {
+        return null;
+    }
+}
