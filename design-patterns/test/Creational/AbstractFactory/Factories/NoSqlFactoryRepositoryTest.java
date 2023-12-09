6ed9fd5d95109553aa329810d1cb6593e5771bd2
Eduardo A. F <eacevedof@yahoo.es>
2023-12-09 23:13:25 +0100

    desing-patterns ya corren los tests

---
+++ design-patterns/test/Creational/AbstractFactory/Factories/NoSqlFactoryRepositoryTest.java
@@ -0,0 +1,26 @@
+package Creational.AbstractFactory.Factories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoSqlFactoryRepositoryTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getInstance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAlumnosRepository() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCursosRepository() {
+    }
+}
