411bd583d09941f7b06cd4ba8c18795f7660299d
Eduardo A. F <eacevedof@yahoo.es>
2023-12-13 12:36:28 +0100

    designs tests ok

--- design-patterns/test/Creational/AbstractFactory/Factories/NoSqlFactoryRepositoryTest.java
+++ design-patterns/test/Creational/AbstractFactory/Factories/NoSqlFactoryRepositoryTest.java
@@ -1,5 +1,9 @@
 package Creational.AbstractFactory.Factories;
 
+import Creational.AbstractFactory.Repositories.Interfaces.AlumnosRepositoryInterface;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 final class NoSqlFactoryRepositoryTest {
@@ -14,13 +18,33 @@ final class NoSqlFactoryRepositoryTest {
 
     @org.junit.jupiter.api.Test
     void getInstance() {
+        assertThat(
+            NoSqlFactoryRepository.getInstance(),
+            instanceOf(NoSqlFactoryRepository.class)
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void wrongInstance() {
+        assertNotEquals(
+            new Object(),
+            instanceOf(AlumnosRepositoryInterface.class)
+        );
     }
 
     @org.junit.jupiter.api.Test
     void getAlumnosRepository() {
+        assertThat(
+            NoSqlFactoryRepository.getInstance().getAlumnosRepository(),
+            instanceOf(AlumnosRepositoryInterface.class)
+        );
     }
 
     @org.junit.jupiter.api.Test
     void getCursosRepository() {
+        assertThat(
+            NoSqlFactoryRepository.getInstance().getAlumnosRepository(),
+            instanceOf(AlumnosRepositoryInterface.class)
+        );
     }
 }
